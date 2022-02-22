70f936bbb8fe3e1d4b941c25fec61a5b3c2eaca7
arthur_ferreira <64388710+arthurferreira061@users.noreply.github.com>
2022-02-22 19:36:17 -0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+include 'mail.php';
+$tempMail = new TempMail();
+
+// $tempMail->refreshTime();
+echo $tempMail->getMail();
+echo "\n";
+// echo $tempMail->getLastMessage();
+// echo "\n";
+// echo $tempMail->getNewMail();
+// echo $tempMail->getLastMessage();
+echo $tempMail->getActivationCode('twitter');
